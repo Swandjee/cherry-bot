215b3f7da3b8dce9c7758307f07c800ee729daa8
Lauri.busa <loki.lightbolt@gmail.com>
2023-11-07 03:05:41 +0100

    action builder

--- commands/role.js
+++ commands/role.js
@@ -1,14 +1,35 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('role')
-		.setDescription('Add yourself (or remove) a role.')
-		.addRoleOption((option) =>
-			option.setName('role').setDescription('The role to add or remove.').setRequired(true),
-		),
+		.setDescription('Add yourself (or remove) a role.'),
 	async execute(interaction) {
-		const role = interaction.options.getRole('role');
-		return interaction.reply({ content: `You selected ${role}`, ephemeral: true });
+
+		const select = new StringSelectMenuBuilder()
+			.setCustomId('starter')
+			.setPlaceholder('Make a selection!')
+			.addOptions(
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Bulbasaur')
+					.setDescription('The dual-type Grass/Poison Seed Pokémon.')
+					.setValue('bulbasaur'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Charmander')
+					.setDescription('The Fire-type Lizard Pokémon.')
+					.setValue('charmander'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Squirtle')
+					.setDescription('The Water-type Tiny Turtle Pokémon.')
+					.setValue('squirtle'),
+			);
+
+		const row = new ActionRowBuilder()
+			.addComponents(select);
+
+		await interaction.reply({
+			content: 'Choose your starter!',
+			components: [row],
+		});
 	},
 };

6f2ab73d13a65b63c8649e8edadb60b84bc53b38
Lauri.busa <loki.lightbolt@gmail.com>
2023-11-07 02:01:09 +0100

    embeds

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -50,9 +50,11 @@ module.exports = {
 				)
 				.setFooter({ text: `Developed by ${dev.username}`, iconURL: myself.displayAvatarURL() });
 			const object = {
-				content: `Hello <@${_member.id}> (${_member.username}), welcome to ${guild.name} !
+				body: {
+					content: `Hello <@${_member.id}> (${_member.username}), welcome to ${guild.name} !
   Make yourself acquainted with the rules below, and use the \`/role\` command in the <#${commandChannel_id}> channel to assign yourself a role.`,
-				embed: [embed],
+					embed: [embed],
+				},
 			};
 			return object;
 		};

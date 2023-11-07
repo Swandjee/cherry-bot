04c6b58f69f8f2c078b01b7bd893c1524e52c721
Lauri.busa <loki.lightbolt@gmail.com>
2023-11-07 13:09:35 +0100

    role mentions

--- commands/eventResponse/stringSelectMenu/roleSelect.js
+++ commands/eventResponse/stringSelectMenu/roleSelect.js
@@ -2,17 +2,48 @@ module.exports = {
 	data: { name: 'roleSelect' },
 	async execute(interaction) {
 
-		const status = interaction.values[0];
+		const roleTag = interaction.values[0];
+
+		const roleMention = `<@&${returnRoleId(roleTag)}>`;
+
+		const member = interaction.member;
+
+		console.log(member.roles);
 
 		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: `Added role ${status}`,
+			await interaction.followUp({ content: `Added role ${roleMention}`,
 				ephemeral: true });
 		}
 		else {
 			await interaction.reply({
-				content: `Added role ${status}`,
+				content: `Added role ${roleMention}`,
 				ephemeral: true,
 			});
 		}
 	},
+};
+
+const returnRoleId = (value) => {
+	switch (value) {
+	case 'nsfw1':
+		return '388348197866700800';
+
+	case 'nsfw2':
+		return '523022272223510549';
+
+	case 'art':
+		return '237919832798920704';
+
+	case 'sl':
+		return '397674427531264001';
+
+	case 'rp':
+		return '237920775515013130';
+
+	case 'voice':
+		return '378161910253617162';
+
+	default:
+		return null;
+	}
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,43 +1,53 @@
 const { Events } = require('discord.js');
-
+const stringSelectPath = '../commands/eventResponse/stringSelectMenu/';
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (interaction.isStringSelectMenu()) {
-			const SelectMenu = require(`../commands/eventResponse/stringSelectMenu/${interaction.customId}.js`);
-			try {
-				await SelectMenu.execute(interaction);
-			}
-			catch (error) {
-				console.error(error);
-				if (interaction.replied || interaction.deferred) {
-					await interaction.followUp({ content: 'There was an error while executing this SelectMenu response!', ephemeral: true });
-				}
-				else {
-					await interaction.reply({ content: 'There was an error while executing this SelectMenu response!', ephemeral: true });
-				}
-			}
-		}
-		if (!interaction.isChatInputCommand()) return;
+		ifStringSelectInteraction(interaction);
 
-		const command = interaction.client.commands.get(interaction.commandName);
+		ifChatInputInteraction(interaction);
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+	},
+};
 
+async function ifStringSelectInteraction(interaction) {
+	if (interaction.isStringSelectMenu()) {
+		const SelectMenu = require(`${stringSelectPath}${interaction.customId}.js`);
 		try {
-			await command.execute(interaction);
+			await SelectMenu.execute(interaction);
 		}
 		catch (error) {
 			console.error(error);
 			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+				await interaction.followUp({ content: 'There was an error while executing this SelectMenu response!', ephemeral: true });
 			}
 			else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				await interaction.reply({ content: 'There was an error while executing this SelectMenu response!', ephemeral: true });
 			}
 		}
-	},
-};
+	}
+}
+
+async function ifChatInputInteraction(interaction) {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+		else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+}
